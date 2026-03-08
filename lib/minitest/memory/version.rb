cb081256935c305b5da8d2db699a717a9b79dc82
Erik Berlin <sferik@gmail.com>
2026-03-07 22:08:20 -0800

    Release v1.1.0

--- lib/minitest/memory/version.rb
+++ lib/minitest/memory/version.rb
@@ -3,6 +3,6 @@ module Minitest # :nodoc:
   # Version information for minitest-memory.
   module Memory
     # Current version of minitest-memory.
-    VERSION = "1.0.0".freeze
+    VERSION = "1.1.0".freeze
   end
 end
